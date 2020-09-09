bb63ca0738cafb259fff8a6b045d0fec2de3460d
Brock Byrd <brockbyrdd@gmail.com>
2020-09-09 18:20:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,10 @@ def countdown(num)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num(1.secs))
-  while num_secs > 0
-    puts "#{num_secs} SECOND(S)!"
-    num_secs -= 1
+def countdown_with_sleep(num(1))
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
   end
   return "HAPPY NEW YEAR!"
 end
